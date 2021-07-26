36d530f0b03e1bc3460d712e8409eda6b4a33a70
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-07-26 21:55:26 +0300

    Added constructor

--- src/main/java/ru/netology/domain/RadioDemo.java
+++ src/main/java/ru/netology/domain/RadioDemo.java
@@ -1,11 +1,6 @@
 package ru.netology.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public class RadioDemo {
-
     private int currentStation;
     private int maxStation = 10;
     private int minStation = 0;
